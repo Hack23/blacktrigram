46c393c944a5c5b37f873cdc6a5fc81a07da3f79
James Pether Sörling <pethers@users.noreply.github.com>
2025-07-11 09:51:43 +0000

    fix: correct indentation in init-xvfb.sh for better readability

--- .devcontainer/init-xvfb.sh
+++ .devcontainer/init-xvfb.sh
@@ -19,14 +19,14 @@ if ! dpkg -s xvfb >/dev/null 2>&1; then
         libxtst6 \
         xauth graphviz libgtk2.0-0 libgtk-3-0 ffmpeg libgbm-dev libnotify-dev fonts-noto fonts-noto-cjk fonts-noto-cjk-extra
 
-    sudo apt-get update \
-        && sudo apt-get install -y wget gnupg \
-        && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
-            | sudo apt-key add - \
-        && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" \
-            > /etc/apt/sources.list.d/google-chrome.list \
-        && sudo apt-get update \
-        && sudo apt-get install -y google-chrome-stable
+   sudo apt-get update \
+    && sudo apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
+        | sudo apt-key add - \
+    && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" \
+        | sudo tee /etc/apt/sources.list.d/google-chrome.list \
+    && sudo apt-get update \
+    && sudo apt-get install -y google-chrome-stable
 
 fi
 
